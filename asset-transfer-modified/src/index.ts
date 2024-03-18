5ed4acdedfced542364e57bb0ede2c6b092dd697
djeelun <71183944+djeelun@users.noreply.github.com>
2024-03-18 02:34:00 +0100

    feat: basic + abac + events

---
+++ asset-transfer-modified/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {AssetTransferContract} from './health-chaincode';
+
+export {AssetTransferContract} from './health-chaincode';
+
+export const contracts: any[] = [AssetTransferContract];
