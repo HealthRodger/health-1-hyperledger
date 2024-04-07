cdac77db34d6b3551631c6b189b0ec23adfbc60e
djeelun <71183944+djeelun@users.noreply.github.com>
2024-04-07 21:47:04 +0200

    feat: date time pickers for time queries

--- health-rodger-frontend/src/dashboard/Researcher/ResearcherPage.js
+++ health-rodger-frontend/src/dashboard/Researcher/ResearcherPage.js
@@ -1,44 +1,37 @@
 import React from "react";
 import GenericQuery from "./GenericQuery";
 import GenericSingleParameterQuery from "./GenericSingleParameterQuery";
-import GenericNoParameterQuery from "./GenericNoParameterQuery";
+// import GenericNoParameterQuery from "./GenericNoParameterQuery";
+import GenericTimeQuery from "./GenericTimeQuery";
 
 export default function ResearcherPage() {
   return (
     <>
       <GenericQuery />
-      <GenericSingleParameterQuery
+      <GenericTimeQuery
         title="List all devices not updated since given timestamp"
         headers={["ID", "Name", "LastUpdate"]}
-        parameterName={"Unix Timestamp"}
-        parameterType={"number"}
         argsToQueryString={(timestamp) => {
           return `{"selector": {"LastUpdate": {"$lt": ${timestamp}}}, "fields": ["ID", "Name", "LastUpdate"]}`;
         }}
       />
-      <GenericSingleParameterQuery
+      <GenericTimeQuery
         title="List all devices updated since given timestamp"
         headers={["ID", "Name", "LastUpdate"]}
-        parameterName={"Unix Timestamp"}
-        parameterType={"number"}
         argsToQueryString={(timestamp) => {
           return `{"selector": {"LastUpdate": {"$gt": ${timestamp}}}, "fields": ["ID", "Name", "LastUpdate"]}`;
         }}
       />
-      <GenericSingleParameterQuery
+      <GenericTimeQuery
         title="List all available devices not updated since given timestamp"
         headers={["ID", "Name", "LastUpdate"]}
-        parameterName={"Unix Timestamp"}
-        parameterType={"number"}
         argsToQueryString={(timestamp) => {
           return `{"selector": {"LastUpdate": {"$lt": ${timestamp}}, "Available": true}, "fields": ["ID", "Name", "LastUpdate"]}`;
         }}
       />
-      <GenericSingleParameterQuery
+      <GenericTimeQuery
         title="List all non-available devices not updated since given timestamp"
         headers={["ID", "Name", "LastUpdate"]}
-        parameterName={"Unix Timestamp"}
-        parameterType={"number"}
         argsToQueryString={(timestamp) => {
           return `{"selector": {"LastUpdate": {"$lt": ${timestamp}}, "Available": false}, "fields": ["ID", "Name", "LastUpdate"]}`;
         }}
